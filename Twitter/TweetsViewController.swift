ae12654ec4c791cff1189da77fc4c2e27b366a70
Nicholas Miller <nickbryanmiller@gmail.com>
2016-02-07 16:16:44 -0500

    Cleaned the code and add user persistance

---
+++ Twitter/TweetsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TweetsViewController.swift
+//  Twitter
+//
+//  Created by Nicholas Miller on 2/7/16.
+//  Copyright © 2016 Nicholas Miller. All rights reserved.
+//
+
+import UIKit
+
+class TweetsViewController: UIViewController {
+
+    var tweets: [Tweet]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        TwitterClient.sharedInstance.homeTimelineWithParams(nil) { (tweets, error) -> () in
+            self.tweets = tweets
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onLogout(sender: AnyObject) {
+        User.currentUser?.logout()
+    }
+    
+}
